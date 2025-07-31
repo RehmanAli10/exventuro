0a0e97868fa4a2b068bd966a7172fee4f3b63480
RehmanAli10 <vrehmanali@gmail.com>
2025-07-31 21:21:28 +0500

    backend route for verifying mobile number

---
+++ src/app/api/verify/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+const accessKey = "67fd230786fc9f9ed9c97e76fbec1c26";
+
+export async function POST(request) {
+  const body = await request.json();
+  // sending request to numverify
+
+  const url = `http://apilayer.net/api/validate?access_key=${accessKey}&number=${body.phone}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.valid) {
+    return NextResponse.json({
+      valid: true,
+      country_code: data.country_code,
+      location: data.location,
+      carrier: data.carrier,
+      line_type: data.line_type,
+    });
+  } else {
+    return NextResponse.json({
+      valid: false,
+      message: "Invalid phone number. Kindly reach out to us directly.",
+    });
+  }
+}
